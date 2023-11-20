aa2c6fd9380b7f1e96163437320483dfb1d2220b
sadeeabdullah <sadeeabdullah016.com>
2023-11-20 12:22:47 +0600

    added verify token

--- Index.js
+++ Index.js
@@ -37,12 +37,26 @@ async function run() {
     const reviewsCollection = client.db("bistrodb").collection("reviews")
     const cartsCollection = client.db("bistrodb").collection("carts")
 
+    // jwt related api
+    app.post( '/jwt', async( req, res ) => {
+      
+      const user = req.body;
+      const token = jwt.sign(
+        user,
+        process.env.ACCESS_TOKEN_SECRET,{
+          expiresIn: '1h'
+        }
+      )
+      res.send( { token } )
+    })
+
     // all method here
 
     // users related api
 
     // posting user doc
     app.post( '/users', async( req, res ) => {
+      
       const user = req.body;
       // insert email if user doesnot exist
       // you can do this in many ways( 1.email unique, 2.upsert, 3. simple checking)
@@ -54,27 +68,73 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result)
     })
+
+    // MIDDLEWARES TO VERIFY TOKEN
+    const verifyToken = (req, res, next) =>{
+      console.log('inside verify token ',req?.headers)
+      if(!req?.headers?.authorization){
+        return res.status(401).send( { message: "unauthorized access" } )
+      }
+      const token = req.headers?.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,( err, decoded ) => {
+        if(err){
+          return res.status(401).send({ message: 'unauthorized Acces' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+     // middleware to verify admin
+     const verifyAdmin = async( req, res, next ) =>{
+      const email = req.decoded.email;
+      const query = { email  :  email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if(!isAdmin){
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
+    // verify its admin or not
+    app.get('/users/admin/:email',verifyToken,async( req,res ) =>{
+      const email = req.params.email;
+      if( email !== req?.decoded?.email){
+        return res.status(403).send({ message: ' forbidden access' })
+      }
+      const query = { email: email};
+      const user = await userCollection.findOne(query)
+      let admin =false;
+      if( user ){
+        admin = user?.role === 'admin'
+      }
+      console.log({admin})
+      res.send({admin})
+    })
+
     
     // getting all the user data
-    app.get( '/users', async( req, res ) => {
+    app.get( '/users',verifyToken,verifyAdmin, async( req, res ) => {
+      
       const result = await userCollection.find().toArray();
       res.send(result)
     })
 
+
+   
 
 
 // cart collection 
     // posting new cart in cart collection
     app.post( '/carts', async( req, res ) => {
       const cartItem = req.body;
-      console.log(cartItem)
       const result = await cartsCollection.insertOne(cartItem);
       res.send( result )
     })
     
     // getting data from cart collection
     app.get( '/carts', async( req, res ) => {
-      const email = req.query.email;
+      const email = req?.query?.email;
+      console.log(email)
       const query = { email: email }
       const result = await cartsCollection.find( query ).toArray();
       res.send(result)
@@ -94,7 +154,7 @@ async function run() {
 
 
     // to delete user from the dashboard
-    app.delete( '/users/:id', async( req, res ) => {
+    app.delete( '/users/:id',verifyToken,verifyAdmin, async( req, res ) => {
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
       const result = await userCollection.deleteOne(query)
@@ -103,7 +163,7 @@ async function run() {
 
 
     // 
-    app.patch('/users/admin/:id', async( req, res ) => {
+    app.patch('/users/admin/:id',verifyToken,verifyAdmin, async( req, res ) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id)};
       const options = {
