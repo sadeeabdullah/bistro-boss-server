0cb528ce4acb2b4c79c3e4e42c23eae59723cb59
sadeeabdullah <sadeeabdullah016.com>
2023-11-21 08:56:29 +0600

    added updateMenu Item methods

--- Index.js
+++ Index.js
@@ -179,10 +179,53 @@ async function run() {
     })
 
     // menu related apis
+    app.post('/menu',verifyToken,verifyAdmin,async( req, res) =>{
+      const item = req.body;
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+    })
     app.get( '/menu', async( req, res ) => {
         const result = await menuCollection.find().toArray();
         res.send(result)
     })
+
+    app.get('/updateItem/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query ={_id:id};
+      const result = await menuCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.patch('/menu/:id', async(req,res)=>{
+      const id = req.params.id;
+      const item = req.bodyj
+      const filter = { _id: id};
+      const updatedDoc={
+        $set:{
+          name:item.name,
+          category: item.category,
+          recipe:item.recipe,
+          image:item.image,
+          price:item.price
+        }
+      }
+      const result = await menuCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+    })
+
+
+    // delete menu
+    app.delete('/menu/:id',async( req, res ) =>{
+      const id = req.params.id;
+      const query ={_id: id}
+      console.log(query)
+      const result = await menuCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
+
+
     app.get( '/reviews', async( req, res ) => {
         const result = await reviewsCollection.find().toArray();
         res.send(result)
